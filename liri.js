2646513b0265532739dea2f3d140b3815ec1c768
Kevin Halstead <mrmichael423@icloud.com>
2019-10-03 21:08:06 -0600

    updated some function

--- liri.js
+++ liri.js
@@ -11,23 +11,34 @@ var search = process.argv.slice(3).join(" ")
 var omdb = "http://www.omdbapi.com/?s=" + search + "&apikey=7d5d5a0a"
 var bands = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
 
-// inquirer.prompt([
-//     {
-//         type: "list",
-//         message: "What would you like to do?",
-//         choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
-//         name: "choice"
-//     }
-// ])
-
+var runApp = function(term) {
+    switch (term) {
+        case "concert-this":
+            concertThis(search)
+            break
+        case "spotify-this-song":
+            spotifyThisSong(search)
+            break
+        case "movie-this":
+            movieThis(search)
+            break
+        case "do-what-it-says":
+            doWhatItSays(search)
+            break
+        default:
+            outputData("That is not a command that I recognize, please try again.") 
+    }
+}
+function concertThis() {
+axios.get(bands).then(function(response) {
+    var info = response.data
+    console.log(info)
+    console.log(`venue name: ${info.venue.name} \nvenue location: ${info.name.city} \n`)
+    })
+}
+concertThis()
 // axios.get(omdb).then(function(response) {
 //     console.log(response.data)
 // })
 
-// axios.get(bands).then(function(response) {
-//     console.log(response.data)
-// })
 
-axios.get(spotify).then(function(response) {
-    console.log(response.tracks)
-})
